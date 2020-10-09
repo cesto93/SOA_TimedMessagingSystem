dc833ef72b1c446277ad39c6669e72dcaae84f56
cesto93 <cesto93@hotmail.it>
2020-10-09 19:00:22 +0200

    added implementation with llist

--- TimedMessagingSystem.c
+++ TimedMessagingSystem.c
@@ -8,6 +8,10 @@
 #include <linux/tty.h>		/* For the tty declarations */
 #include <linux/version.h>	/* For LINUX_VERSION_CODE */
 
+
+#include <linux/workqueue.h>
+#include <linux/slab.h>
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Pier Francesco Contino");
 
@@ -23,20 +27,65 @@ static int Major;	/* Major number assigned to broadcast device driver */
 #define get_major(session)	MAJOR(session->f_inode->i_rdev)
 #define get_minor(session)	MINOR(session->f_inode->i_rdev)
 
-typedef struct _object_state{
-	struct mutex operation_synchronizer;
-	int valid_bytes;
-	char * stream_content;	//the I/O node is a buffer in memory
+typedef struct msg_node {
+	char * msg;
+	int len;
+	struct llist_node node;
+} msg_node;
 
-} object_state;
+typedef struct _msg_state{
+	struct mutex operation_synchronizer;
+	struct llist_head * msg_list;
+	struct llist_head * pending_list;
+	long read_timeout;
+	long write_timeout;
+} msg_state;
 
 #define MINORS 8
-object_state objects[MINORS];
+msg_state msg_buffers[MINORS];
 #define OBJECT_MAX_SIZE  (4096) //just one page
 
 #define SET_SEND_TIMEOUT 0
 #define SET_RECV_TIMEOUT 1
 #define REVOKE_DELAYED_MESSAGES 2
+
+// PENDING WRITE DATA
+typedef struct pending_data_node {
+	msg_state * msg_storage;
+	char * msg;
+	int len;
+	struct llist_node node;
+} pending_data_node;
+
+typedef struct work_data{
+	struct llist_head * pending_list;
+	struct delayed_work my_work;
+} work_data;
+
+struct workqueue_struct * queue;
+
+void deffered_write(struct delayed_work *work) {
+	work_data * data = container_of(work, work_data, my_work);
+	struct llist_head * list = data->pending_list;
+	struct llist_node * node = llist_del_first(list);
+	pending_data_node * pending_data = llist_entry(node, pending_data_node, node);
+
+	msg_node * new = kmalloc(sizeof(msg_node), GFP_KERNEL);
+	new->msg = pending_data->msg;
+	new->len = pending_data->len; 
+
+	llist_add(&new->node, pending_data->msg_storage->msg_list);
+
+	/*int ret = copy_from_user(r_node->source, r_node->msg_buffer->buffer, r_node->len);
+	if (ret != r_node->len) {
+		printk("deffered_write can't write all the data");
+	}*/
+
+
+
+	kfree(pending_data);
+	kfree(data);
+}
 
 // the actual driver
 
@@ -62,27 +111,54 @@ static int dev_release(struct inode *inode, struct file *file) {
 static ssize_t dev_write(struct file *filp, const char *buff, size_t len, loff_t *off) {
 	int minor = get_minor(filp);
 	int ret;
-	object_state *the_object;
+	msg_state *my_msg_buffer;
+	pending_data_node * pending_node;
+	work_data * wdata;
 
-	the_object = objects + minor;
+	my_msg_buffer = msg_buffers + minor;
 	printk("%s: somebody called a write on dev with [major,minor] number [%d,%d]\n",MODNAME,get_major(filp),get_minor(filp));
 
-	mutex_lock(&(the_object->operation_synchronizer));
-	if(*off >= OBJECT_MAX_SIZE) {	//offset too large
-		mutex_unlock(&(the_object->operation_synchronizer));
+	mutex_lock(&(my_msg_buffer->operation_synchronizer));
+
+
+	/*if(*off >= OBJECT_MAX_SIZE) {	//offset too large
+		mutex_unlock(&(my_msg_buffer->operation_synchronizer));
 		return -ENOSPC;	//no space left on device
 	} 
-	if(*off > the_object->valid_bytes) {	//offset beyond the current stream size
-		mutex_unlock(&(the_object->operation_synchronizer));
+	if(*off > my_msg_buffer->valid_bytes) {	//offset beyond the current stream size
+		mutex_unlock(&(my_msg_buffer->operation_synchronizer));
 		return -ENOSR;	//out of stream resources
 	} 
 	if((OBJECT_MAX_SIZE - *off) < len) 
-		len = OBJECT_MAX_SIZE - *off;
-	ret = copy_from_user(&(the_object->stream_content[*off]),buff,len);
+		len = OBJECT_MAX_SIZE - *off;*/
+
+	if (len > OBJECT_MAX_SIZE)
+		len = OBJECT_MAX_SIZE;		
+
+
+	//work_queue
+	if (my_msg_buffer->write_timeout == 0) {
+		msg_node * node = kmalloc(sizeof(msg_node), GFP_KERNEL);
+		node->msg = kmalloc(len, GFP_KERNEL); 
+		ret = copy_from_user(node->msg, buff, len);	
+		node->len = ret;
+
+		llist_add(&node->node, my_msg_buffer->msg_list);
+	} else {
+		pending_node = kmalloc(sizeof(pending_data_node), GFP_KERNEL);
+		pending_node->msg = kmalloc(len, GFP_KERNEL);
+		ret = copy_from_user(pending_node->msg, buff, len);
+		pending_node->len = ret;
+
+		wdata = kmalloc(sizeof(pending_data_node), GFP_KERNEL);
+		wdata->pending_list = my_msg_buffer->pending_list;
+		INIT_DELAYED_WORK(&wdata->my_work, (void *) deffered_write);
+		queue_delayed_work(queue, &wdata->my_work, my_msg_buffer->write_timeout);
+	}
+
   
-	*off += (len - ret);
-	the_object->valid_bytes = *off;
-	mutex_unlock(&(the_object->operation_synchronizer));
+	//*off += (len - ret);
+	mutex_unlock(&(my_msg_buffer->operation_synchronizer));
 
 	return len - ret;
 }
@@ -90,35 +166,51 @@ static ssize_t dev_write(struct file *filp, const char *buff, size_t len, loff_t
 static ssize_t dev_read(struct file *filp, char *buff, size_t len, loff_t *off) {
 	int minor = get_minor(filp);
 	int ret;
-	object_state *the_object;
+	msg_state * msg_buffer;
+	struct llist_node * my_node;
+	msg_node * my_msg_node;
 
-	the_object = objects + minor;
+	msg_buffer = msg_buffers + minor;
 	printk("%s: somebody called a read on dev with [major,minor] number [%d,%d]\n", MODNAME, get_major(filp), get_minor(filp));
 
-	mutex_lock(&(the_object->operation_synchronizer));
-	if (*off > the_object->valid_bytes) {
-		mutex_unlock(&(the_object->operation_synchronizer));
+	mutex_lock(&(msg_buffer->operation_synchronizer));
+
+	//get the msg
+	my_node = llist_del_first(msg_buffer->msg_list);
+	my_msg_node = llist_entry(my_node, msg_node, node);
+	if (len > my_msg_node->len)
+		len = my_msg_node->len;
+
+	/*if (*off > msg_len) {
+		mutex_unlock(&(msg_buffer->operation_synchronizer));
 		return 0;
 	} 
-	if ((the_object->valid_bytes - *off) < len) 
-		len = the_object->valid_bytes - *off;
-	ret = copy_to_user(buff,&(the_object->stream_content[*off]),len);
+	if ((msg_len - *off) < len) 
+		len = msg_len - *off;*/
+
+	
+	ret = copy_to_user(buff, &my_msg_node->msg, len);
   
-	*off += (len - ret);
-	mutex_unlock(&(the_object->operation_synchronizer));
+	//*off += (len - ret);
+	mutex_unlock(&(msg_buffer->operation_synchronizer));
 
 	return len - ret;
 }
 
 static long dev_ioctl(struct file *filp, unsigned int command, unsigned long param) {
 	int minor = get_minor(filp);
-	object_state *the_object;
+	msg_state *msg_buffer;
 
-	the_object = objects + minor;
+	msg_buffer = msg_buffers + minor;
+	switch(command) {
+		case SET_SEND_TIMEOUT: msg_buffer->write_timeout = param ; break;
+		case SET_RECV_TIMEOUT: msg_buffer->read_timeout = param; break;
+		case REVOKE_DELAYED_MESSAGES: /*remove pending msg*/; break;
+		default: return -1;
+	}
 	printk("%s: somebody called an ioctl on dev with [major,minor] number [%d,%d] and command %u \n", 
 			MODNAME, get_major(filp), get_minor(filp), command);
 
-	//do here whathever you would like to cotrol the state of the device
 	return 0;
 }
 
@@ -139,18 +231,20 @@ static struct file_operations fops = {
 
 int init_module(void) {
 	int i;
+
 	//initialize the drive internal state
 	for(i=0; i < MINORS; i++) {
-		mutex_init(&(objects[i].operation_synchronizer));
-		objects[i].valid_bytes = 0;
-		objects[i].stream_content = NULL;
-		objects[i].stream_content = (char*)__get_free_page(GFP_KERNEL);
-		if(objects[i].stream_content == NULL) {
-			for (; i>=0; i--)
-				free_page((unsigned long)objects[i].stream_content);
-			return -ENOMEM;
-		}
+		mutex_init(&(msg_buffers[i].operation_synchronizer));
+
+		msg_buffers[i].read_timeout = 0;
+		msg_buffers[i].write_timeout = 0;
+
+		msg_buffers[i].msg_list = kmalloc(sizeof(struct llist_head), GFP_KERNEL);
+		init_llist_head(msg_buffers[i].msg_list);
+
 	}
+	queue = alloc_workqueue("timed-msg-system", WQ_UNBOUND, 32);
+
 	Major = __register_chrdev(0, 0, 256, DEVICE_NAME, &fops);	//allowed minors are directly controlled within this driver
 
 	if (Major < 0) {
@@ -165,7 +259,8 @@ int init_module(void) {
 void cleanup_module(void) {
 	int i;
 	for(i=0; i < MINORS; i++) {
-		free_page((unsigned long)objects[i].stream_content);
+		//TODO free msg list
+
 	}
 
 	unregister_chrdev(Major, DEVICE_NAME);
