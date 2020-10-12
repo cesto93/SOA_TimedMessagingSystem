8eb654ee8d82f0675fe6b6de5d8c481ac16a9df1
cesto93 <cesto93@hotmail.it>
2020-10-12 18:15:23 +0200

    testing sys fs

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 obj-m += TimedMessagingSystem.o
+obj-m += sys.o
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules 
---
+++ sys.c
@@ -0,0 +1,76 @@
+#include <linux/module.h> 
+#include <linux/moduleparam.h> 
+#include <linux/printk.h> 
+#include <linux/kobject.h> 
+#include <linux/sysfs.h> 
+#include <linux/init.h> 
+#include <linux/fs.h> 
+#include <linux/string.h>
+
+
+#define NAME "SYS BASELINE MODULE"
+
+static struct kobject *example_kobject;
+static int max_msg_size;
+static int max_storage_size;
+
+module_param(max_msg_size, int, 0660);
+module_param(max_storage_size, int, 0660);
+
+
+static ssize_t max_msg_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf) {
+//just one page of storage from buff
+        return sprintf(buf, "%d\n", max_msg_size);
+}
+
+static ssize_t max_msg_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count) {
+//just one page of storage from buff
+        sscanf(buf, "%du", &max_msg_size);
+        return count;
+}
+
+static ssize_t max_storage_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf) {
+//just one page of storage from buff
+        return sprintf(buf, "%d\n", max_storage_size);
+}
+
+static ssize_t max_storage_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count) {
+//just one page of storage from buff
+        sscanf(buf, "%du", &max_storage_size);
+        return count;
+}
+
+
+static struct kobj_attribute msg_attribute = __ATTR(max_msg_size, 0660, max_msg_show, max_msg_store);
+static struct kobj_attribute storage_attribute = __ATTR(max_storage_size, 0660, max_storage_show, max_storage_store);
+
+static int __init mymodule_init (void) {
+        int error = 0;
+        printk("%s: initialization\n", NAME);
+
+        example_kobject = kobject_create_and_add(NAME, kernel_kobj); 
+        //the kernel_kobj variable points to the /sys/kernel object
+        if(!example_kobject)
+                return -ENOMEM;
+
+        error = sysfs_create_file(example_kobject, &msg_attribute.attr); //actually creating another kernel object
+        if (error) {
+                printk("%s: failed to create the target kobj\n",NAME);
+                return error;
+        }
+        error = sysfs_create_file(example_kobject, &storage_attribute.attr); //actually creating another kernel object
+        if (error) {
+                printk("%s: failed to create the target kobj\n",NAME);
+        }
+        return error;
+}
+
+static void __exit mymodule_exit (void)
+{
+        printk("%s: shutdown \n",NAME);
+        kobject_put(example_kobject);
+}
+
+MODULE_LICENSE("GPL"); 
+module_init(mymodule_init);
+module_exit(mymodule_exit);
